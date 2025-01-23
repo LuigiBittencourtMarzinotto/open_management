09d390f06c4715b0768c71b9feabfe8a4b7d4e4b
Luigi-Bittencourt-Marzinotto <luigi.marzinotto@genesisgroup.com.br>
2025-01-22 22:12:45 -0300

    Subindo projeto

--- resources/views/app/show-user.blade.php
+++ resources/views/app/show-user.blade.php
@@ -1,5 +1,4 @@
 @extends('app.layouts.app')
 @section('content')
-
 <show-user-component codigo-user={{ $codigoUser }} route-verified-cep="{{ route('verified-cep') }}" route-user={{ route('user.show', [$codigoUser])  }}></show-user-component>
 @endsection
